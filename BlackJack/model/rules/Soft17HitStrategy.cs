6b98abf1aa5719451536ae74d033a15785f6a277
Daniel Vedin <dv222bk@student.lnu.se>
2015-10-22 17:32:07 +0200

    Soft17HitRule
    Implemented a soft 17 hit rule. With this rule, the dealer will draw a
    card if his hand consists of an ace and other cards with a total value
    of 6.

---
+++ BlackJack/model/rules/Soft17HitStrategy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.model.rules
+{
+    class Soft17HitStrategy : IHitStrategy
+    {
+        private const int g_hitLimit = 17;
+
+        public bool DoHit(model.Player a_dealer)
+        {
+            if (a_dealer.CalcScore() < g_hitLimit)
+            {
+                return true;
+            }
+            else if (a_dealer.CalcScore() == g_hitLimit)
+            {
+                int totalValue = 0;
+                bool aceFound = false;
+                foreach (Card card in a_dealer.GetHand())
+                {
+                    if (card.GetValue() == Card.Value.Ace)
+                    {
+                        aceFound = true;
+                    } else {
+                        totalValue += (int)card.GetValue();
+                        if (totalValue > 6)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                if (totalValue == 6 && aceFound)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
